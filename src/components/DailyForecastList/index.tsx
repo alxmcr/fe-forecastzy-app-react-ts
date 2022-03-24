4c3777b2d5f0fbee5595efbef16d3f2e0eec42c0
Alejandro M. Coca <alxmcr@outlook.com>
2022-03-24 13:15:26 -0400

    feat(components): created the DailyForecastList component

---
+++ src/components/DailyForecastList/index.tsx
@@ -0,0 +1,25 @@
+import { IDailyForecast } from "../../@types/appTypes";
+import DailyForecastCard from "../DailyForecastCard";
+
+interface IDailyForecastListProps {
+  dailyForecasts: IDailyForecast[];
+}
+
+export default function DailyForecastList({
+  dailyForecasts = [],
+}: IDailyForecastListProps) {
+  if (dailyForecasts.length === 0) {
+    return <p>No daily forecasts</p>;
+  }
+
+  return (
+    <div className="forecasts">
+      {dailyForecasts.map((dailyForecast) => (
+        <DailyForecastCard
+          key={dailyForecast.dt}
+          dailyForecast={dailyForecast}
+        />
+      ))}
+    </div>
+  );
+}
