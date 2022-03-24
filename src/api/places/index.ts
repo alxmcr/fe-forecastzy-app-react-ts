3b56323f6e1cdb29a753dab7c990ce4b54b1e508
Alejandro M. Coca <alxmcr@outlook.com>
2022-03-24 19:04:35 -0400

    feat(api): added the specific logic to handlerPlaces function

--- src/api/places/index.ts
+++ src/api/places/index.ts
@@ -1,7 +1,21 @@
-interface IPlacesRequest {
-    query: string;
-}
+import { IPlacesRequest, IPlacesURLParams } from "../../@types/apiTypes";
+import { generateURLPlaces } from "../../helpers/apiHelpers";
+import { IPlace } from "../../@types/appTypes";
 
-export default function handleSearchCity({ query = "" }: IPlacesRequest) {
-    console.log("Searching for city: " + query);
-};
+export async function handlerPlaces(request: IPlacesRequest): Promise<IPlace[]> {
+    const { signal, query } = request
+    const requestOptions: RequestInit = {
+        method: 'GET',
+        signal: signal,
+    };
+
+    const urlParams: IPlacesURLParams = { query }
+    const url = generateURLPlaces(urlParams);
+    const response: Response = await fetch(url, requestOptions)
+
+    if (response.status === 200) {
+        return response.json();
+    }
+
+    return Promise.reject(response);
+}
