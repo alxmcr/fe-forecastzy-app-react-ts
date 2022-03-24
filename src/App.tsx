ff30cf999c325c146fbfc9fcb8dc51e83274b71b
Alejandro M. Coca <alxmcr@outlook.com>
2022-03-24 14:13:38 -0400

    feat(app): added DailyForecastSection as component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import CitySearchCard from "./components/CitySearchCard";
+import DailyForecastSection from "./components/DailyForecastSection";
 
 function App() {
   return (
     <main className="App">
       <h1>Forecastzy</h1>
       <CitySearchCard />
+      <DailyForecastSection />
     </main>
   );
 }
