91eb9e69bf559e7c622eb5cd092789aa288e8ff1
Alejandro M. Coca <alxmcr@outlook.com>
2022-03-24 19:50:12 -0400

    feat(styles): created the skeleton files to Sass files

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import "./App.css";
+import "./App.scss";
 import CitySearchCard from "./components/CitySearchCard";
 import DailyForecastSection from "./components/DailyForecastSection";
 
 function App() {
   return (
     <main className="App">
-      <h1>Forecastzy</h1>
+      <h1 className="App__title">Forecastzy</h1>
       <CitySearchCard />
       <DailyForecastSection />
     </main>
