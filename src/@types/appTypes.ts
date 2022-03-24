5a6c9daf4bddb6bc6d26cb829ddb7bc87cdb44fa
Alejandro M. Coca <alxmcr@outlook.com>
2022-03-24 14:10:13 -0400

    feat(types): added IPlace interface

--- src/@types/appTypes.ts
+++ src/@types/appTypes.ts
@@ -37,3 +37,20 @@ export interface IForecastWeather {
     description: string,
     icon: string
 }
+
+export interface IPlace {
+    id: number,
+    slug: string,
+    city_slug: string,
+    display: string,
+    ascii_display: string,
+    city_name: string,
+    city_ascii_name: string,
+    state: string,
+    country: string,
+    lat: string,
+    long: string,
+    result_type: string,
+    popularity: string,
+    sort_criteria: number
+}
