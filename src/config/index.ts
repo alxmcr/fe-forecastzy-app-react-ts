cdbbd3c8850535227884400e66cc5e3b48a82e62
Alejandro M. Coca <alxmcr@outlook.com>
2022-03-24 18:02:40 -0400

    feat(app): added config file

---
+++ src/config/index.ts
@@ -0,0 +1,9 @@
+export interface IAppConfig {
+    API_KEY_OPEN_WEATHER_MAP: string;
+    ENDPOINT_OPEN_WEATHER_MAP_ONE_CALL_API: string;
+}
+
+export const appConfig: IAppConfig = {
+    API_KEY_OPEN_WEATHER_MAP: process.env.REACT_APP_OPEN_WEATHER_MAP_API_KEY || "fake-api-key",
+    ENDPOINT_OPEN_WEATHER_MAP_ONE_CALL_API: "https://api.openweathermap.org/data/2.5/onecall" || "",
+}
