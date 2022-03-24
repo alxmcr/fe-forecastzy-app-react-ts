2342e05005c002a608913a1f346d4a52dce1d5de
Alejandro M. Coca <alxmcr@outlook.com>
2022-03-24 18:45:18 -0400

    feat(config): added the endpoint to places api

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,11 @@
 export interface IAppConfig {
     API_KEY_OPEN_WEATHER_MAP: string;
     ENDPOINT_OPEN_WEATHER_MAP_ONE_CALL_API: string;
+    ENDPOINT_PLACES_API: string;
 }
 
 export const appConfig: IAppConfig = {
     API_KEY_OPEN_WEATHER_MAP: process.env.REACT_APP_OPEN_WEATHER_MAP_API_KEY || "fake-api-key",
-    ENDPOINT_OPEN_WEATHER_MAP_ONE_CALL_API: "https://api.openweathermap.org/data/2.5/onecall" || "",
+    ENDPOINT_OPEN_WEATHER_MAP_ONE_CALL_API: "https://api.openweathermap.org/data/2.5/onecall",
+    ENDPOINT_PLACES_API: "https://search.reservamos.mx/api/v2/places",
 }
