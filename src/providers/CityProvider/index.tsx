275220c0043530e6a481ba3bda101ceb8d7aac9a
Alejandro M. Coca <alxmcr@outlook.com>
2022-03-24 14:11:27 -0400

    feat!(provider): added 'city' as property in our CityProvider provider

--- src/providers/CityProvider/index.tsx
+++ src/providers/CityProvider/index.tsx
@@ -1,22 +1,28 @@
 import { createContext, FC, useState } from "react";
+import { IPlace } from "../../@types/appTypes";
 
 interface ICityProviderState {
   cityName: string;
   setCityName: (cityName: string) => void;
+  city: IPlace | undefined;
+  setCity: (city: IPlace | undefined) => void;
 }
 
 const initialState: ICityProviderState = {
   cityName: "",
   setCityName: (cityName: string) => {},
+  city: undefined,
+  setCity: (city: IPlace | undefined) => {},
 };
 
 export const CityContext = createContext(initialState);
 
 export const CityProvider: FC = ({ children }) => {
   const [cityName, setCityName] = useState("");
+  const [city, setCity] = useState<IPlace | undefined>(undefined);
 
   return (
-    <CityContext.Provider value={{ cityName, setCityName }}>
+    <CityContext.Provider value={{ cityName, setCityName, city, setCity }}>
       {children}
     </CityContext.Provider>
   );
