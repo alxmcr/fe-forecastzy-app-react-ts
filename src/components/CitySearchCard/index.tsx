f62247ddd7ab0437b79406d511e32ead6e612582
Alejandro M. Coca <alxmcr@outlook.com>
2022-03-24 19:08:10 -0400

    feat(component): implemented the handleCitySearchForm logic

--- src/components/CitySearchCard/index.tsx
+++ src/components/CitySearchCard/index.tsx
@@ -1,20 +1,50 @@
 import { useContext } from "react";
-import handleSearchCity from "../../api/places";
+import { IPlacesRequest, StatusOperation } from "../../@types/apiTypes";
+import handlerCity from "../../api/cities";
 import { CityContext } from "../../providers/CityProvider";
 import CitySearchForm from "../CitySearchForm";
 
 export default function CitySearchCard() {
-  const { cityName } = useContext(CityContext);
+  const {
+    cityName,
+    setCity,
+    statusCity,
+    setStatusCity,
+    errorMessageCity,
+    setErrorMessageCity,
+  } = useContext(CityContext);
 
-  const handleCitySearchForm = (event: React.FormEvent<HTMLFormElement>) => {
+  const handleCitySearchForm = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    handleSearchCity({ query: cityName });
+
+    let abortController: AbortController | null = null;
+    try {
+      abortController = new AbortController();
+      const request: IPlacesRequest = {
+        signal: abortController.signal,
+        query: cityName,
+      };
+      setStatusCity(StatusOperation.PENDING);
+      const cityFound = await handlerCity(request);
+      setCity(cityFound);
+      setStatusCity(StatusOperation.SUCCESS);
+    } catch (error) {
+      setStatusCity(StatusOperation.ERROR);
+      if (error instanceof Error) {
+        setErrorMessageCity(error.message);
+      }
+    } finally {
+      if (abortController) {
+        abortController.abort();
+      }
+    }
   };
 
   return (
     <article className="search-card">
       <h2 className="search-card__title">Search for a city</h2>
       <CitySearchForm onSearchCity={handleCitySearchForm} />
+      {statusCity === StatusOperation.ERROR ? <p>{errorMessageCity}</p> : null}
     </article>
   );
 }
