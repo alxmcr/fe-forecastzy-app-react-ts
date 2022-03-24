9fe238f12933ccce01b5237b88039f2a76f754ba
Alejandro M. Coca <alxmcr@outlook.com>
2022-03-24 13:54:27 -0400

    feat(app): added the CitySearchCard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import './App.css';
+import "./App.css";
+import CitySearchCard from "./components/CitySearchCard";
 
 function App() {
   return (
     <main className="App">
       <h1>Forecastzy</h1>
+      <CitySearchCard />
     </main>
   );
 }
