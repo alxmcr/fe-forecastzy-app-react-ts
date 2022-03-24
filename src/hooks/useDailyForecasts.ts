c84256d18e2aa77d70efb6307a1b9b70ab7a52e2
Alejandro M. Coca <alxmcr@outlook.com>
2022-03-24 19:06:06 -0400

    feat(hooks): added more logic to useDailyForecasts

--- src/hooks/useDailyForecasts.ts
+++ src/hooks/useDailyForecasts.ts
@@ -1,9 +1,9 @@
 import { useEffect, useState } from "react";
 import { IForecastsResponse, StatusOperation } from "../@types/apiTypes";
-import { IDailyForecast, IUseForecastsParams } from "../@types/appTypes";
+import { IDailyForecast, IPlace } from "../@types/appTypes";
 import handlerForecasts from "../api/forecasts";
 
-export default function useDailyForecasts({ latitude = 0, longitude = 0, units = "metric" }: IUseForecastsParams) {
+export default function useDailyForecasts(place: IPlace | null | undefined) {
     const [forecasts, setForecasts] = useState<IDailyForecast[]>([]);
     const [statusForecasts, setStatusForecasts] = useState<StatusOperation>(StatusOperation.IDLE);
     const [errorMessageForecasts, setErrorMessageForecasts] = useState<string>("");
@@ -11,7 +11,7 @@ export default function useDailyForecasts({ latitude = 0, longitude = 0, units =
     useEffect(() => {
         let abortController: AbortController | null = null;
 
-        const fetchData = async () => {
+        const fetchData = async (place: IPlace) => {
             setStatusForecasts(StatusOperation.PENDING);
             setErrorMessageForecasts("");
 
@@ -19,9 +19,9 @@ export default function useDailyForecasts({ latitude = 0, longitude = 0, units =
                 abortController = new AbortController();
                 const response: IForecastsResponse = await handlerForecasts({
                     signal: abortController.signal,
-                    latitude,
-                    longitude,
-                    units,
+                    latitude: Number(place.lat),
+                    longitude: Number(place.long),
+                    units: "metric",
                     exclude: "hourly,minutely,alerts",
                 });
 
@@ -37,14 +37,16 @@ export default function useDailyForecasts({ latitude = 0, longitude = 0, units =
             }
         };
 
-        fetchData();
+        if (place !== undefined && place !== null) {
+            fetchData(place);
+        }
 
         return () => {
             if (abortController !== null) {
                 abortController.abort();
             }
         };
-    }, [latitude, longitude, units]);
+    }, [place]);
 
     return { forecasts, statusForecasts, errorMessageForecasts };
 }
